67630258d1bb9d3459cad3c952b067a9450e2253
dnisko <dnisko@hotmail.com>
2026-05-23 02:37:16 +0200

    Fixed OrderItem

--- DomainModels/OrderItem.cs
+++ DomainModels/OrderItem.cs
@@ -8,8 +8,8 @@
         public int ProductId { get; set; }
         public Product Product { get; set; } = null!;
 
-        public decimal Quantity { get; set; }
-        public string Unit { get; set; } = string.Empty;
+        public int Quantity { get; set; }
+        //public string Unit { get; set; } = string.Empty;
         public decimal Price { get; set; }
     }
 }
